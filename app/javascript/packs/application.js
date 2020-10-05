7e648e3cf06f8bfa09a6841988d7353ec1ccdac1
Wisdom Praise <wisdompraise968@gmail.com>
2020-10-05 11:02:44 +0100

    Drag & Drop Sorting with sortable JS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+import Sortable from "sortablejs";
 
 import "../stylesheets/application"
 
@@ -17,3 +18,9 @@ import "../stylesheets/application"
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+document.addEventListener('turbolinks:load',() => {
+    var el = document.getElementById("tasks-list");
+    var sortable = Sortable.create(el, {
+        animation: 150
+    });
+})
--- app/models/project.rb
+++ app/models/project.rb
@@ -17,11 +17,23 @@ class Project < ApplicationRecord
     return 'not-started' if tasks.none?
     if tasks.all? { |task| task.complete? }
       'complete'
-    elsif tasks.any? {|task| task.in_progress? }
+    elsif tasks.any? { |task| task.in_progress? || task.complete? }
       'in-progress'
     else
       'not-started'
     end
   end
 
+  def percent_complete
+    return 0 if tasks.none?
+    ((total_complete.to_f / total_tasks) * 100).round
+  end
+
+  def total_complete 
+    complete_task = tasks.select { |task| task.complete? }.count
+  end
+
+  def total_tasks
+    tasks.count
+  end
 end
